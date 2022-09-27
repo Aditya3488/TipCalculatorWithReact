021b895ba35319a36dd1d015120649060ca4c5a3
AdityaNarayanTiwari <104133590+Aditya3488@users.noreply.github.com>
2022-09-27 20:02:50 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './App.css';
+import Header from './Components/Header';
+import Input from './Components/Input';
+import Output from './Components/Output';
+import Footer from './Components/Footer';
+
+
+function App() {
+  return (
+    <main className="App">
+
+      <Header />
+      <Input />
+      <Output />
+      <Footer />
+    
+    </main>
+  );
+}
+
+export default App;
+
